540dfcd29f29284dff1d9f4ac7d54daef9c7c252
ipc103 <iancandysemail@gmail.com>
2017-01-24 13:18:38 -0500

    initial commit

---
+++ javascripts/01_myForEach.js
@@ -0,0 +1,8 @@
+const cats = [
+    {name: 'Maru', age: 9},
+    {name: 'Mooskers', age: 7},
+    {name: 'Grumpy Cat', age: 12}
+  ]
+
+// Build a function, myForEach, that takes an array and a callback function.
+// For each element in the array, execute the callback function and pass in the element, index, and original array
---
+++ javascripts/03_arrowFunctions.js
@@ -0,0 +1,14 @@
+class Cat {
+
+  constructor(){
+    this.kittens = ["Marley", "Grouchy", "Billy" ]
+  }
+
+  feedCats(){
+    // iterate over our list of kittens and for each one, call our 'feedKitten' function
+  }
+
+  feedKitten(kitten){
+    console.log(`Feeding ${kitten}`)
+  }
+}
